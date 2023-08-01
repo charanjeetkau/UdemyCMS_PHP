38069a72ecfe00756fa1c92917552c439bcecefe
Charanjeet Kaur <61412684+charanjeetkau@users.noreply.github.com>
2023-08-01 10:26:29 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,23 @@
+        <!-- Footer -->
+        <footer>
+             <div class="row bg-secondary">
+                <div class="col-lg-12">
+                    <p class="text-center text-muted">Copyright &copy; Our CMS Website 2023</p>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+       </footer>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,56 @@
+
+<?php include "includes/admin_header.php";?>
+
+    <div id="wrapper">
+
+
+
+        <!-- Navigation -->
+
+        <?php include "includes/admin_navigation.php";?>
+
+
+
+
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                            WELCOME to Admin Page!
+                            <small>Author</small>
+                        </h1>
+
+                        
+                        <?php //include "categories.php"; ?>
+
+<!--                        <ol class="breadcrumb">
+                            <li>
+                                <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
+                            </li>
+                            <li class="active">
+                                <i class="fa fa-file"></i> Some Page
+                            </li>
+                        </ol>
+ -->
+
+                    </div>
+                </div>
+                <!-- /.row -->
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+
+
+        <?php include "includes/admin_footer.php";?>
+
+
+
+
+
